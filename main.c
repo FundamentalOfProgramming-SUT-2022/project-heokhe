ab5e553eb3fdb2ec72dcb9d9ba3dd5d0bf567bb2
Hosein Khansari <hrk.x11@gmail.com>
2023-01-21 18:51:08 +0330

    feat: automatically create undo history directory

--- main.c
+++ main.c
@@ -68,6 +68,9 @@ char* cat(char* address) {
 }
 
 void write_with_history(char* address, char* content) {
+  if (!_exists(UNDO_HISTORY)) {
+    mkdir(UNDO_HISTORY, 0700);
+  }
   write_to_file(get_prev_version_name(address), cat(address));
   write_to_file(address, content);
 }
